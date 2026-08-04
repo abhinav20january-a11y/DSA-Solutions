1810470e6edfb555bb3a5d04c5a09d4c3159c13a
abhinav sharma <250694650+abhinav20january-a11y@users.noreply.github.com>
2026-08-04 23:52:33 +0530

    solve(hash-tables): add Find Missing Elements

--- hash-tables/3731-find-missing-elements/README.md
+++ hash-tables/3731-find-missing-elements/README.md
@@ -5,7 +5,7 @@
 - **Patterns:** Array, Hash Table, Sorting
 - **Status:** Completed
 - **Problem:** [Open original](https://leetcode.com/problems/find-missing-elements/)
-- **Completed:** 2026-08-04T16:57:10.793Z
+- **Completed:** 2026-08-04T18:22:30.187Z
 
 ## Approach
 
--- hash-tables/3731-find-missing-elements/solution.cpp
+++ hash-tables/3731-find-missing-elements/solution.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
+        int s=*min_element(nums.begin(),nums.end());
+        int m=*max_element(nums.begin(),nums.end());
+        for(int i=s;i<=m;i++){
+            bool found=false;
+            for(int j=0;j<nums.size();j++){
+                if(i==nums[j]){
+                    found=true;
+                    break;
+
+
+                }
+
+            }
+            if(!found){
+                ans.push_back(i);
+            }
+        }
+        ans.sort();
+        return ans;
+
         
     }
 };
