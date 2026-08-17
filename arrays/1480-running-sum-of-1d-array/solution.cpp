0d29ceca2a9e621fb10a3ea97791d75163bc397d
abhinav sharma <250694650+abhinav20january-a11y@users.noreply.github.com>
2026-08-17 13:49:14 +0530

    solve(arrays): add Running Sum of 1d Array

--- arrays/1480-running-sum-of-1d-array/solution.cpp
+++ arrays/1480-running-sum-of-1d-array/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> arr(nums.size());
 
         int sum = 0;
 
         for(int i = 0; i < nums.size(); i++) {
             sum += nums[i];
-            arr[i] = sum;
+            nums[i] = sum;
         }
 
-        return arr;
+        return nums;
     }
 };
