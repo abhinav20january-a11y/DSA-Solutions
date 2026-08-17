c0b34e64da77b2102904a2f2e6ea148585e0b009
abhinav sharma <250694650+abhinav20january-a11y@users.noreply.github.com>
2026-08-17 21:37:38 +0530

    solve(stacks-queues): add Next Greater Element I

--- stacks-queues/0496-next-greater-element-i/README.md
+++ stacks-queues/0496-next-greater-element-i/README.md
@@ -5,7 +5,7 @@
 - **Patterns:** Array, Hash Table, Stack, Monotonic Stack
 - **Status:** Completed
 - **Problem:** [Open original](https://leetcode.com/problems/next-greater-element-i/)
-- **Completed:** 2026-08-13T15:47:09.576Z
+- **Completed:** 2026-08-17T16:05:55.476Z
 
 ## Approach
 
--- stacks-queues/0496-next-greater-element-i/solution.cpp
+++ stacks-queues/0496-next-greater-element-i/solution.cpp
@@ -2,8 +2,42 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n=nums2.size();
-        for(int i=n-1;)
+        int arr[n];
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]){
+                st.pop();
+                }
+            
+            if(st.empty())
+                arr[i]=-1;
+
+            else
+                {
+                    arr[i]=st.top();
+                    
+
+                }
+                st.push(nums2[i]);
+        
+
+            }
+            vector<int>ans;
+            for(int i=0;i<nums1.size();i++){
+                for(int j=0;j<n;j++){
+                    if(nums1[i]==nums2[j]){
+                        ans.push_back(arr[j]);
+                        break;
+                    }
+                }
+            }
+
+              return ans;
+
+
+        }
+      
 
         
-    }
+    
 };
