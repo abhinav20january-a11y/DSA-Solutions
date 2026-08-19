3be296d01c0512359db7e97a17b7dc2830211325
abhinav sharma <250694650+abhinav20january-a11y@users.noreply.github.com>
2026-08-19 14:20:06 +0530

    solve(dynamic-programming): add Sum of Subarray Minimums

--- dynamic-programming/0907-sum-of-subarray-minimums/solution.cpp
+++ dynamic-programming/0907-sum-of-subarray-minimums/solution.cpp
@@ -3,12 +3,13 @@ public:
     int sumSubarrayMins(vector<int>& arr) {
         int n=arr.size();
         int mod=(int)(1e9+7);
-        int mini=INT_MAX;
+        
         int sum=0;
         for(int i=0;i<n;i++){
+            int mini=INT_MAX;
             for(int j=i;j<n;j++){
                 mini=min(mini,arr[j]);
-                sum+=mini%mod;
+                sum=(sum+mini)%mod;
                 
 
             }
